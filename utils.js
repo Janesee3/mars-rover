605d961dcb5b721c56e035492bea6f35135095a9
See Loo Jane <muigek34@yahoo.com.sg>
2018-08-20 16:24:38 +0800

    add parsing methods and test cases

--- utils.js
+++ utils.js
@@ -1,9 +1,20 @@
 const { NORTH, SOUTH, EAST, WEST, POS_X, POS_Y } = require("./constants");
 
 const DIRECTION_INDEX = 2;
+const EXPECTED_POSITION_INPUT_LENGTH = 3;
+const EXPECTED_SIZE_INPUT_LENGTH = 2;
 
 const parsePositionInput = positionInput => {
 	let characterArray = positionInput.split(" ");
+
+	if (characterArray.length !== EXPECTED_POSITION_INPUT_LENGTH) return null;
+
+	const posX = new Number(characterArray[POS_X]);
+	const posY = new Number(characterArray[POS_Y]);
+	const direction = characterArray[DIRECTION_INDEX];
+
+	if (!_isLegalPosition(posX, posY)) return null;
+	if (!_isLegalDirection(direction)) return null;
 
 	return {
 		position: [
@@ -14,6 +25,34 @@ const parsePositionInput = positionInput => {
 	};
 };
 
+const _isLegalPosition = (posX, posY) => {
+	return posX instanceof Number && posY instanceof Number;
+};
+
+const _isLegalDirection = direction => {
+	const arrayOfDirections = [NORTH, EAST, SOUTH, WEST];
+
+	return arrayOfDirections.includes(direction);
+};
+
+const _isLegalSize = (maxX, maxY) => {
+	return (
+		maxX instanceof Number && maxY instanceof Number && maxX > 0 && maxY > 0
+	);
+};
+
+const parseSizeInput = sizeInput => {
+	let characterArray = positionInput.split(" ");
+	if (characterArray.length != EXPECTED_SIZE_INPUT_LENGTH) return null;
+	const maxX = new Number(characterArray[POS_X]);
+	const maxY = new Number(characterArray[POS_Y]);
+
+	if (!_isLegalSize(maxX, maxY)) return null;
+
+	return [maxX, maxY];
+};
+
 module.exports = {
-	parsePositionInput
+	parsePositionInput,
+	parseSizeInput
 };
--- utils.test.js
+++ utils.test.js
@@ -2,12 +2,27 @@ const { parsePositionInput } = require("./utils");
 const { NORTH, SOUTH, EAST, WEST } = require("./constants");
 
 describe("Test for parsePositionInput()", () => {
-	it("should return an object with correct position and direction given a valid input", () => {
+	it.only("should return an object with correct position and direction given a valid input", () => {
 		const expected = {
 			position: [1, 2],
 			direction: NORTH
 		};
 
 		expect(parsePositionInput("1 2 N")).toEqual(expected);
+	});
+
+	it("should return null if input is incomplete", () => {
+		expect(parsePositionInput("1 N")).toEqual(null);
+		expect(parsePositionInput("")).toEqual(null);
+		expect(parsePositionInput("11N")).toEqual(null);
+		expect(parsePositionInput("1 1 N N")).toEqual(null);
+	});
+
+	it("should return null if input is not formatted correctly", () => {
+		expect(parsePositionInput("1 1 1")).toEqual(null);
+		expect(parsePositionInput("A A N")).toEqual(null);
+		expect(parsePositionInput("1 A N")).toEqual(null);
+		expect(parsePositionInput("A 1 N")).toEqual(null);
+		expect(parsePositionInput("1 1 G")).toEqual(null);
 	});
 });
